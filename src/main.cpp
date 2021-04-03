d953638a3cf777eecb3aacdf030f474abe2f61ef
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-04-03 19:13:35 +1300

    Maybe working version of the staff

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ FASTLED_USING_NAMESPACE
 #endif
 
 #define NUM_STRIPS 1
-#define NUM_LEDS_PER_STRIP 600
+#define NUM_LEDS_PER_STRIP 223 //26+30 +26+30+55+26+30
 #define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
 CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
@@ -15,6 +15,9 @@ CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 #define BRIGHTNESS          20
 #define FRAMES_PER_SECOND  120
 
+
+int potval = 0;
+bool runleds = true;
 //Forward Declare Functions
 void nextPattern();
 void rainbow();
@@ -52,7 +55,7 @@ void setup() {
   delay(200); // 3 second delay for recovery
 
   // tell FastLED there's 60 NEOPIXEL leds on pin 3, starting at index 0 in the led array
-  FastLED.addLeds<NEOPIXEL, 13>(leds, 0, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<NEOPIXEL, 23>(leds, 0, NUM_LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
@@ -65,15 +68,21 @@ void setup() {
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
 //SimplePatternList gPatterns = { rainbow, confetti, sinelon, juggle, bpm };
-SimplePatternList gPatterns = { rainbow};
+SimplePatternList gPatterns = { confetti, rainbow};
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 void loop()
 {
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
+  if(runleds){
+    // Call the current pattern function once, updating the 'leds' array
+    gPatterns[gCurrentPatternNumber]();
+  }
+  else{
+    fadeToBlackBy( leds, NUM_LEDS, 10);  
+  }
+  
 
   // send the 'leds' array out to the actual LED strip
   FastLED.show();
@@ -81,8 +90,31 @@ void loop()
   FastLED.delay(1000/FRAMES_PER_SECOND);
 
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 1 ) { gHue = gHue+3; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 20 ) { nextPattern(); } // change patterns periodically
+  EVERY_N_MILLISECONDS( 1 ) { gHue = gHue+3; 
+  } // slowly cycle the "base color" through the rainbow
+  EVERY_N_MILLISECONDS ( 100 ){
+    potval = analogRead(10);
+    if(potval <=250){
+     runleds=false;
+     FastLED.setBrightness(0);
+   }
+   else if(potval<=500){
+     gCurrentPatternNumber=0;
+     FastLED.setBrightness(BRIGHTNESS);
+     runleds=true;
+   }
+   else if(potval<=750){  // rainbow dim
+    gCurrentPatternNumber=1;
+    FastLED.setBrightness(BRIGHTNESS);
+    runleds=true;
+   }
+   else{   //rainbow bright
+        gCurrentPatternNumber=1;
+        FastLED.setBrightness(50);
+        runleds=true;
+   }
+  }
+  // EVERY_N_SECONDS( 20 ) { nextPattern(); } // change patterns periodically
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
